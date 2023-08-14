707c63a52d9aacc90361ef477f6561e1928d5989
Alfian Andi Nugraha <me@alfianandi.dev>
2023-08-14 21:04:01 +0700

    feat(gulpfile.mjs): create task gulp

---
+++ gulpfile.mjs
@@ -0,0 +1,47 @@
+import gulp from "gulp";
+import babel from "@rollup/plugin-babel";
+import { rollup } from "rollup";
+import file from "gulp-file";
+import { glob } from "glob";
+import typescript from "@rollup/plugin-typescript";
+import { deleteAsync } from "del";
+import terser from "@rollup/plugin-terser";
+
+gulp.task("clean", () => {
+  return deleteAsync(["dist/**/*"]);
+});
+
+gulp.task("generate:typescript", () => {
+  return rollup({
+    input: glob.sync("src/**/*.{ts,tsx}"),
+    plugins: [
+      babel({
+        presets: ["@babel/preset-env", "@babel/preset-react"],
+        babelHelpers: "bundled",
+      }),
+      typescript(),
+      terser(),
+    ],
+    external: ["react", "react-dom"],
+  })
+    .then((bundle) => {
+      return bundle.generate({
+        format: "esm",
+        dir: "dist/",
+        sourcemap: true,
+        preserveModules: true,
+      });
+    })
+    .then((gen) => {
+      const files = gen.output.map((file) => {
+        return {
+          name: file.fileName.replace("dist/", "").replace("types/", ""),
+          source: file.code ?? file.source,
+        };
+      });
+
+      return file(files).pipe(gulp.dest("dist/esm/"));
+    });
+});
+
+gulp.task("build", gulp.series("clean", "generate:typescript"));
