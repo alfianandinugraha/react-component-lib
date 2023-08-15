32e34ef16ff8760a9a4ce386f81b336bb7e01a45
Alfian Andi Nugraha <me@alfianandi.dev>
2023-08-15 12:44:42 +0700

    fix(index.tsx): using src/index.tsx as entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,3 @@
-import React from "react";
-import { useModal } from "./hooks";
-import { Button, Input } from "@/components";
-
-const App = () => {
-  useModal();
-  return (
-    <div>
-      <Input />
-      <Button>Hello</Button>
-      <h1>React App</h1>
-    </div>
-  );
-};
-
-export default App;
+export * from "./components";
+export * from "./hooks";
+export * from "./utils";
